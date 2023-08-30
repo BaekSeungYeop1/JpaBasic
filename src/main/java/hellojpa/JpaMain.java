c42a5556587cd2d0865805f87ae9e21bbe0f8e1b
DESKTOP-NT05U6L\Yeop <tmdduq5498@gmail.com>
2023-08-30 23:50:11 +0900

    영속성 컨테스트 - 영속,비영속 & 엔티티 등록,수정

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -13,15 +15,24 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-//            Member findMember = em.find(Member.class,1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-            
-            for (Member member : result){
-                System.out.println("member.getName() = " + member.getName());
-            }
+//
+            // 비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+
+            // 영속
+            // 1차 캐시에 저장됨
+            System.out.println("=======BEFORE========");
+            em.persist(member);
+            System.out.println("=======AFTER========");
+
+            //1차 캐시에서 조회
+            Member findMember1 = em.find(Member.class, 100L);
+            Member findMember2 = em.find(Member.class, 100L);
+
+            // 영속 엔티티의 영속성 보장
+            System.out.println("result = " + (findMember1==findMember2));
             tx.commit();
         }catch (Exception e){
             tx.rollback();
