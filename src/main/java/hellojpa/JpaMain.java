8b7c56fcbf7f9fb375327b045f30651fa078ff45
DESKTOP-NT05U6L\Yeop <tmdduq5498@gmail.com>
2023-09-02 23:48:57 +0900

    스키마 자동 생성 및 필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,28 +16,7 @@ public class JpaMain {
         tx.begin();
         try{
 //
-            // 비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
 
-            // 영속
-            // 1차 캐시에 저장됨
-            System.out.println("=======BEFORE========");
-            em.persist(member);
-            System.out.println("=======AFTER========");
-
-            // 준영속 상태
-            em.detach(member); // 특정 엔티티만 준영속 상태로 전환
-            em.clear(); // 영속성 컨텍스트를 완전히 초기화
-            em.close(); // 영속성 컨테스트 종료
-
-            //1차 캐시에서 조회
-            Member findMember1 = em.find(Member.class, 100L);
-            Member findMember2 = em.find(Member.class, 100L);
-
-            // 영속 엔티티의 영속성 보장
-            System.out.println("result = " + (findMember1==findMember2));
             tx.commit();
         }catch (Exception e){
             tx.rollback();
